d080a5e747522f58f3528af6303242324ecec61d
MissLENA123 <157268921+elenor123@users.noreply.github.com>
2024-01-25 06:16:53 +0200

    added JS to the search engine

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-alert("hello ");
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#searchForm");
+searchFormElement.addEventListener("submit", searchCity);
